433c5b1ed042c255a3ab3bb2af2f879440ae8631
Aiden <hyena.shenanigans@gmail.com>
2022-08-01 16:02:38 -0700

    Adds testing && user role && user token

--- src/auth/accountHandler.js
+++ src/auth/accountHandler.js
@@ -2,24 +2,16 @@
 let base64 = require('base-64');
 const bcrypt = require('bcrypt');
 const { Account } = require('../db');
+const { app } = require('../server');
+//const SECRET = 'pog' // JWT encoded signature as env WRONG? plaintext JWT password
 
-//let string = 'someusername:P@55w0rD!';
-//let encoded = base64.encode(string); // c29tZXVzZXJuYW1lOlBANTV3MHJEIQ==
-//let decoded = base64.decode(encoded); // someusername:P@55w0rD!
-
-
-//let password = 'supersecret';
-
-// Create a new hashed password
-//bcrypt.hash(password, 5).then(hashedPassword => { console.log(hashedPassword) });
-// Validate a password
-//bcrypt.compare(password, hashedPassword).then(isValid => { console.log(isValid) });
-const signup = async (req, res) => { // TODO: Build in protection against duplicate usernames
+const signup = async (req, res) => {
   try {
     req.body.password = base64.decode(req.body.password);
-    req.body.password = await bcrypt.hash(req.body.password, 3);
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+    req.body.role = 'admin';
     const record = await Account.create(req.body);
-    res.status(201).send('Successful account creation!');
+    res.status(201).send({ username: req.body.username, password: req.body.password });
   } catch (error) {
     res.status(500).send('Error creating account');
     console.log('Account creation failed, error:', error);
@@ -27,17 +19,21 @@ const signup = async (req, res) => { // TODO: Build in protection against duplic
 }
 
 const login = async (req, res) => {
+  // jwt.io
+  // TODO: get jetbrains mono
+  // TODO: build in authorization logic to CRUD as middleware
   try {
     const password = await Account.findOne({where: { username: req.body.username }});
     //console.log('Database password found: ',password.password)
-    req.body.password = base64.decode(req.body.password);
+    //req.body.password = base64.decode(req.body.password);
     //console.log('Post decode password', req.body.password);
     if (bcrypt.compare(req.body.password, password.password)){
-      res.status(200).send('Successful signin, INSERT TOKEN HERE');
+      res.status(200).send({username: req.body.username, token: process.env.SECRET });
     } else {
       res.status(403).send('Incorrect login information');
     }
   } catch (error) {
+    console.log(error);
     res.status(500).send('Error logging in');
     console.log('Server error when logging in:', error);
   }
